bbc42d065c3634133d7bbd05e4a0e0c472e79a8e
Dereckchavez <dc5067@bard.edu>
2016-08-17 10:25:42 -0400

    Update eliza.js

--- workshop1/eliza.js
+++ workshop1/eliza.js
@@ -30,71 +30,54 @@
 var rules = {
   "~*x hello ~*y": [
     "How do you do. Please state your problem."
+    "Wassup bro"
+    "Que lo que my knee
+    "YAOOOOOOO"
+    "What up"
   ],
   "~*x computer ~*y": [
-    "Do computers worry you?",
-    "What do you think about machines have to do with your problem?",
-    "Don't you think computers can help people",
-    "Why do you mention computers?",
-    "What about computers worries you?",
-    "What do you think about computers?",
-    "What do you think machines have to do with your problem?",
+    ",
   ],
   "~*x name ~*y": [
-    "I am not interested in names",
+ ,
+    "DON'T EVEN SAY NOTHING TO ME BOY YOU LOOK LIKE A MOTHERFUCKING UGHHHHHHHHHHH"
   ],
   "~*x sorry ~*y": [
-    "Please don't apologize",
-    "Apologies are not necessary",
-    "What feelings do you have when you apologize",
+ ,
+    "Boy if you don't get yo goofy ass outta here"
+  
   ],
   "~*x I remember ~*y": [
-    "Do you often think of ~y?",
-    "Does thinking of ~y bring anything else to mind?",
-    "What else do you remember?",
-    "Why do you recall ~y right now?",
-    "What in the present situation reminds you of ~y?",
-    "What is the connection between me and ~y?",
+,
   ],
   "~*x do you remember ~*y": [
-    "Did you think I would forget ~y?",
-    "Why do you think I should recall ~y now?",
-    "What about ~y?",
-    "You mentioned ~y",
+ ,
   ],
   "~*x I want ~*y": [
-    "What would it mean if you got ~y?",
-    "Why do you want ~y?",
-    "Suppose you got ~y soon.",
+  
+    "I wanna rock with somebody"
   ],
     "~*x I need ~*y": [
-    "What would it mean if you got ~y?",
-    "Why do you need ~y?",
-    "Suppose you got ~y soon.",
+ 
+    "Your love and affection"
   ],
   "~*x if ~*y": [
-    "Do you really think it's likely that ~y?",
-    "Do you wish that ~y?",
-    "What do you think about ~y?",
-    "Really--if ~y?",
+   ,
+    "If I was a boy"
   ],
   "~*x I dreamt ~*y": [
-    "Really-- ~y",
-    "Have you ever fantasized ~y while you were awake",
-    "Have you dreamt of ~y before?",
+  ,
   ],
   "~*x I dream about ~*y": [
-    "How do you feel about ~y in reality?",
+  ,
+    "I dream about getting that bread bro nothing gonna stop me fam"
   ],
   "~*x dream ~*y": [
-    "What does this dream suggest to you?",
-    "Do you dream often?",
-    "What persons appear in your dreams?",
-    "Don't you believe that dream has to do with your problem?",
+    ",
   ],
   "~*x my mother ~*y": [
-    "Who else in your family ~y?",
-    "Tell me more about your family",
+   ",
+    "You cant stay here. Go to your fathers house"
   ],
   "~*x my father ~*y": [
     "Your father?",
@@ -102,22 +85,16 @@ var rules = {
     "What else comes to mind when you think of your father?",
   ],
   "~*x I am glad ~*y": [
-    "How have I helped you to be ~y?",
-    "What makes you happy just now?",
-    "Can you explain why you are suddenly ~y?",
+    ",
   ],
   "~*x I am sad ~*y": [
-    "I am sorry to hear you are depressed",
-    "I'm sure it's not pleasant to be sad",
+   ",
   ],
   "~*x are like ~*y": [
     "What resemblence do you see between ~x and ~y?",
   ],
   "~*x is like ~*y": [
-    "In what way is it that ~x is like ~y?",
-    "What resemblence do you see?",
-    "Could there really be some connection?",
-    "How?",
+    
   ],
   "~*x alike ~*y": [
     "In what way?",
@@ -127,59 +104,44 @@ var rules = {
     "What other connections do you see?",
   ],
   "~*x no ~*y": [
-    "Why not?",
-    "You are being a bit negative.",
-    "Are you saying 'No' just to be negative?",
+    "
+    "Aint no way im gonna let you down",
   ],
   "~*x I was ~*y": [
-    "Were you really?",
-    "Perhaps I already knew you were ~y.",
-    "Why do you tell me you were ~y now?",
+ ,
+    "Ooooo im about to cut that ass bro"
   ],
   "~*x was I ~*y": [
-    "What if you were ~y?",
-    "Do you think you were ~y?",
-    "What would it mean if you were ~y?",
+    ",
   ],
   "~*x I am ~*y": [
-    "In what way are you ~y?",
-    "Do you want to be ~y?",
+    
+	
   ],
   "~*x am I ~*y": [
-    "Do you believe you are ~y?",
-    "Would you want to be ~y?",
-    "You wish I would tell you you are ~y?",
-    "What would it mean if you were ~y?",
+    ",
   ],
   "~*x am ~*y": [
     "Why do you say 'AM?'",
     "I don't understand that",
   ],
   "~x are ~*y": [
-    "Did you think they might not be ~y?",
-    "Possibly they are ~y",
+  ,
   ],
    "~*x are you ~*y": [
-    "Why are you interested in whether I am ~y or not?",
-    "Would you prefer if I weren't ~y ?",
-    "Perhaps I am ~y in your fantasies.",
+   .",
   ],
   "~*x you are ~*y": [
-    "What makes you think I am ~y?",
+    ,
   ],
   "~*x because ~*y": [
-    "Is that the real reason?",
-    "What other reasons might there be?",
-    "Does that reason seem to explain anything else?",
+   ",
   ],
   "~*x were you ~*y": [
-    "Perhaps I was ~y?",
-    "What do you think?",
-    "What if I had been ~y?",
+    ,
   ],
   "~*x I can't ~*y": [
-    "Maybe you could ~y now",
-    "What if you could ~y?",
+   ,
   ],
   "~*x I feel ~*y": [
     "Do you often feel ~y?"
@@ -191,39 +153,30 @@ var rules = {
     "Perhaps in your fantasy we ~y each other",
   ],
   "~*x why don't you ~*y": [
-    "Should you ~y yourself?",
-    "Do you believe I don't ~y ?",
-    "Perhaps I will ~y in good time",
+    
   ],
   "~*x yes ~*y": [
-    "You seem quite positive",
-    "You are sure?",
-    "I understand",
+  
+    "BOY IF YO DONT GET YO GOOFY ASS OUTTA HERE"
   ],
   "~*x someone ~*y": [
-    "Can you be more specific?",
+
+    "They don't like you"
   ],
   "~*x everyone ~*y": [
-    "Surely not everyone",
-    "Can you think of anyone in particular?",
-    "Who, for example?",
-    "You are thinking of a special person",
+   ,
   ],
   "~*x always ~*y": [
-    "Can you think of a specific example?",
-    "When?",
-    "What incident are you thinking of?",
-    "Really--always?",
+  
   ],
   "~*x what ~*y": [
     "Why do you ask?",
-    "Does that question interest you?",
-    "What is it you really want to know?",
-    "What do you think?",
-    "What comes to your mind when you ask that?",
+   ,
   ],
   "~*x perhaps ~*y": [
-    "You do not seem quite certain",
+  
+    "I look like a damn Dr.Phil to you"
+    "YOU DONT GOT THE ANSWERS SWAY"
   ],
 };
 
